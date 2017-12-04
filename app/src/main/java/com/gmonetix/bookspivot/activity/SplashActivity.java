9bfb703c8df59c9d3c407120ee92c261c1b6852d
ThisIsNSH <hadanis.singh@gmail.com>
2017-12-04 16:20:22 +0530

    gradient+animation

--- app/src/main/java/com/gmonetix/bookspivot/activity/SplashActivity.java
+++ app/src/main/java/com/gmonetix/bookspivot/activity/SplashActivity.java
@@ -1,5 +1,8 @@
 package com.gmonetix.bookspivot.activity;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Handler;
@@ -7,6 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.gmonetix.bookspivot.R;
 
@@ -26,6 +33,10 @@ public class SplashActivity extends AppCompatActivity {
             Window w = getWindow(); // in Activity's onCreate() for instance
             w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
+
+        ImageView logo = (ImageView) findViewById(R.id.logo);
+        Animation logo_anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        logo.startAnimation(logo_anim);
 
         new Handler().postDelayed(new Runnable() {
 
