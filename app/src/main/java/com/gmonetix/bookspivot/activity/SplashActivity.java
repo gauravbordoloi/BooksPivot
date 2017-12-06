061c2afbb54590c0169aa91d90750d71c923d74c
ThisIsNSH <hadanis.singh@gmail.com>
2017-12-06 21:25:57 +0530

    dimension error fixed

--- app/src/main/java/com/gmonetix/bookspivot/activity/SplashActivity.java
+++ app/src/main/java/com/gmonetix/bookspivot/activity/SplashActivity.java
@@ -49,7 +49,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                Intent i = new Intent(SplashActivity.this, Home.class);
                 startActivity(i);
 
                 // close this activity
