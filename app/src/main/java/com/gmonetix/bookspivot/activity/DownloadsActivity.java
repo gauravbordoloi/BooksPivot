4020a0da939d4ad8178d64309144132d5dca4417
ThisIsNSH <hadanis.singh@gmail.com>
2017-12-14 12:24:04 +0530

    assign adapter to listview

--- app/src/main/java/com/gmonetix/bookspivot/activity/DownloadsActivity.java
+++ app/src/main/java/com/gmonetix/bookspivot/activity/DownloadsActivity.java
@@ -38,6 +38,9 @@ public class DownloadsActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
     }
 
 }
