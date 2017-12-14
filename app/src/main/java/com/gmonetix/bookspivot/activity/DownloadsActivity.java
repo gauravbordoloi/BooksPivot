a38f859189a5217fd510ca991acaceedcaa10b78
ThisIsNSH <hadanis.singh@gmail.com>
2017-12-14 12:40:01 +0530

    list implemented

--- app/src/main/java/com/gmonetix/bookspivot/activity/DownloadsActivity.java
+++ app/src/main/java/com/gmonetix/bookspivot/activity/DownloadsActivity.java
@@ -2,8 +2,11 @@ package com.gmonetix.bookspivot.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.gmonetix.bookspivot.R;
 
@@ -35,34 +38,23 @@ public class DownloadsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_downloads);
 
+        CustomList adapter = new
+                CustomList(DownloadsActivity.this, book, imageId);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-        // Get ListView object from xml
-        ListView listView = (ListView) findViewById(R.id.list);
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(DownloadsActivity.this, "You Clicked at " +book[+ position], Toast.LENGTH_SHORT).show();
 
-        // Defined Array values to show in ListView
-        String[] values = new String[] { "Android List View",
-                "Adapter implementation",
-                "Simple List View In Android",
-                "Create List View Android",
-                "Android Example",
-                "List View Source Code",
-                "List View Array Adapter",
-                "Android Example List View"
-        };
+            }
+        });
 
-        // Define a new Adapter
-        // First parameter - Context
-        // Second parameter - Layout for the row
-        // Third parameter - ID of the TextView to which the data is written
-        // Forth - the Array of data
+    }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
-        // Assign adapter to ListView
-        listView.setAdapter(adapter);
-
-
+}
 
     }
 
