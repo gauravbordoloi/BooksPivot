7e9abae23d0fef79bd0e1726e6637eae76be3f78
ThisIsNSH <hadanis.singh@gmail.com>
2017-12-15 14:32:30 +0530

    variables created

--- app/src/main/java/com/gmonetix/bookspivot/activity/FragmentList.java
+++ app/src/main/java/com/gmonetix/bookspivot/activity/FragmentList.java
@@ -1,8 +1,48 @@
 package com.gmonetix.bookspivot.activity;
 
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.gmonetix.bookspivot.R;
+
 /**
  * Created by this-is-nsh on 15/12/17.
  */
 
-public class FragmentList {
+public class FragmentList extends ArrayAdapter<String> {
+
+    private final Activity context;
+    private final String[] name;
+    private final String[] upload;
+    private final String[] download;
+    private final Integer[] imageId;
+
+    public FragmentList(Activity context,
+                        String[] name, Integer[] imageId, String[]  upload, String[]  download ) {
+        super(context, R.layout.list_single, name );
+        this.context = context;
+        this.name = name;
+        this.upload= upload;
+        this.download= download;
+        this.imageId = imageId;
+
+    }
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView= inflater.inflate(R.layout.list_fragment, null, true);
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.txt);
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.txt);
+        TextView txtTitle = (TextView) rowView.findViewById(R.id.);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
+        txtTitle.setText(web[position]);
+
+        imageView.setImageResource(imageId[position]);
+        return rowView;
+    }
 }
